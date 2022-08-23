cf036f22f6dbd35e5bdc89a8aa3caf2ff84cbcf0
Vyacheslav Beatum <podarkiprod@gmail.com>
2022-08-23 18:06:36 +0300

    added "Defanging An IP Adress" task

---
+++ 1108.DefangingAnIPAddress/Program.cs
@@ -0,0 +1,11 @@
+﻿string s = "1.1.1.1";
+
+string res = DefangIPaddr(s);
+Console.WriteLine(res);
+
+
+string DefangIPaddr(string address)
+{
+    string s = address.Replace(".", "[.]");
+    return s;
+}
