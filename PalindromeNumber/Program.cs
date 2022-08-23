1b63ad23923313438ecfe0625b471e5142786c9c
Vyacheslav Beatum <podarkiprod@gmail.com>
2022-08-23 15:53:01 +0300

    Добавьте файлы проекта.

---
+++ PalindromeNumber/Program.cs
@@ -0,0 +1,19 @@
+﻿
+int i = 121;
+IsPalindrome(i);
+
+
+bool IsPalindrome(int x)
+{
+    string num = Convert.ToString(x);
+
+    char[] numsArr = num.ToCharArray();
+    Array.Reverse(numsArr);
+
+    string reverseNum = new string(numsArr);
+
+    if (num == reverseNum)
+        return true;
+    else
+        return false;
+}
