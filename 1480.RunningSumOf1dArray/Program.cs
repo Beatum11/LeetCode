a7c7b95b8ea7800b4651ba2b65465359d277bfe5
Vyacheslav Beatum <podarkiprod@gmail.com>
2022-08-23 16:37:55 +0300

    added task "Running Sum of 1d Array"

---
+++ 1480.RunningSumOf1dArray/Program.cs
@@ -0,0 +1,13 @@
+﻿int[] nums = { 2, 34, 5, 2, 5 };
+int[] resultArr = RunningSum(nums);
+
+
+int[] RunningSum(int[] nums)
+{
+    for (int i = 1; i < nums.Length; i++)
+    {
+        nums[i] += nums[i - 1];
+    }
+
+    return nums;
+}
