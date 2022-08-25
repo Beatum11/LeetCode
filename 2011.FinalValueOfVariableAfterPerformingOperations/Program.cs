2aa2ed54c3dbc03c88315e32ad85ecdd3cb3788f
Vyacheslav Beatum <podarkiprod@gmail.com>
2022-08-25 18:44:15 +0300

    added two tasks

---
+++ 2011.FinalValueOfVariableAfterPerformingOperations/Program.cs
@@ -0,0 +1,35 @@
+﻿string[] commands = { "X++", "X--", "++X", "--X" };
+
+int i = FinalValueAfterOperations(commands);
+
+int FinalValueAfterOperations(string[] operations)
+{
+    int x = 0;
+
+    for (int i = 0; i < operations.Length; i++)
+    {
+        switch (operations[i])
+        {
+            case "X++":
+                x += 1;
+                break;
+
+            case "++X":
+                x += 1;
+                break;
+
+            case "X--":
+                x -= 1;
+                break;
+
+            case "--X":
+                x -= 1;
+                break;
+
+            default:
+                break;
+        }
+    }
+
+    return x;
+}
---
+++ 771.JewelsAndStones/Program.cs
@@ -0,0 +1,24 @@
+﻿string jewels = "aA";
+string stones = "aAAbbbb";
+
+int i = NumJewelsInStones(jewels, stones);
+
+
+int NumJewelsInStones(string jewels, string stones)
+{
+    int num = 0;
+
+    char[] jArr = jewels.ToCharArray();
+    char[] stArr = stones.ToCharArray();
+
+    for (int i = 0; i < jArr.Length; i++)
+    {
+        for (int j = 0; j < stArr.Length; j++)
+        {
+            if (jArr[i] == stArr[j])
+                num++;
+        }
+    }
+
+    return num;
+}
