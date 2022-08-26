40f3a7f1e3c7b635d410374b396d73989a9e63be
Vyacheslav Beatum <podarkiprod@gmail.com>
2022-08-26 14:51:27 +0300

    added 557. task

---
+++ 557.ReverseWordsInAStringIII/Program.cs
@@ -0,0 +1,20 @@
+﻿string z = "Good Day";
+
+string ReverseWords(string s)
+{
+    string[] words = s.Split(' ');
+    string[] finalArr = new string[words.Length];
+
+    for (int i = 0; i < words.Length; i++)
+    {
+        char[] chars = words[i].ToCharArray();
+        Array.Reverse(chars);
+        finalArr[i] += new string(chars);
+    }
+
+    string sFinal = string.Join(' ', finalArr);
+    return sFinal;
+}
+
+string final = ReverseWords(z);
+Console.WriteLine(final);
