f76f49439e30cf68ab861ff2dc42bac16c22fe92
Vyacheslav Beatum <podarkiprod@gmail.com>
2022-08-26 14:25:00 +0300

    added 1816. task

---
+++ 1816.TruncateSentence/Program.cs
@@ -0,0 +1,19 @@
+﻿string s = "Hello how are you bitch";
+int k = 4;
+
+string TruncateSentence(string s, int k)
+{
+    string[] final = new string[k];
+    string[] arr = s.Split(' ');
+
+    for (int i = 0; i < final.Length; i++)
+    {
+        final[i] = arr[i];
+    }
+
+    string c = string.Join(' ', final);
+    return c;
+}
+
+string final = TruncateSentence(s, k);
+Console.WriteLine(final);
