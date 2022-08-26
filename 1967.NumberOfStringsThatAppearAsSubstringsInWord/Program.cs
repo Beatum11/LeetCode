e805844d3a91e94a0f4bdfbfd9528d8b7fd29f00
Vyacheslav Beatum <podarkiprod@gmail.com>
2022-08-26 12:16:18 +0300

    added 1967. task

---
+++ 1967.NumberOfStringsThatAppearAsSubstringsInWord/Program.cs
@@ -0,0 +1,18 @@
+﻿string[] patterns = { "ab", "b", "c" };
+string word = "aaaaabbbbb";
+
+int NumOfStrings(string[] patterns, string word)
+{
+    int res = 0;
+
+    foreach (string pattern in patterns)
+    {
+        if (word.Contains(pattern))
+            res++;
+    }
+
+    return res;
+}
+
+int res = NumOfStrings(patterns, word);
+Console.WriteLine(res);
