12ba18f813db417104df4879728a76411f637507
Vyacheslav Beatum <podarkiprod@gmail.com>
2022-08-27 13:28:14 +0300

    added 657. task

---
+++ 657.RobotReturnToOrigin/Program.cs
@@ -0,0 +1,47 @@
+﻿string moves = "UD";
+
+int[] ReturnToOrigign(string moved)
+{
+    int[] loc = { 0, 0 };
+    char[] chars = moved.ToCharArray();
+
+    foreach (char c in chars)
+    {
+        switch (c)
+        {
+            case 'U':
+                loc[1] += 1;
+                break;
+
+            case 'D':
+                loc[1] -= 1;
+                break;
+
+            case 'L':
+                loc[0] -= 1;
+                break;
+
+            case 'R':
+                loc[0] += 1;
+                break;
+
+            default:
+                break;
+        }
+    }
+
+    return loc;
+}
+
+bool JudgeCircle(string moves)
+{
+    int[] final = ReturnToOrigign(moves);
+
+    if (final[0] == 0 && final[1] == 0)
+        return true;
+    else
+        return false;
+}
+
+bool final = JudgeCircle(moves);
+Console.WriteLine(final);
