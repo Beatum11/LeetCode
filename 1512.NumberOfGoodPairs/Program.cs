a605a62b0c47f32245f01ce5e6a7b0445665b0be
Vyacheslav Beatum <podarkiprod@gmail.com>
2022-08-27 12:18:07 +0300

    added 1512. task

---
+++ 1512.NumberOfGoodPairs/Program.cs
@@ -0,0 +1,22 @@
+﻿int[] nums = { 1, 1, 1, 1 };
+
+int NumIdenticalPairs(int[] nums)
+{
+    int res = 0;
+
+    for (int i = 0; i < nums.Length; i++)
+    {
+        for (int j = i + 1; j < nums.Length; j++)
+        {
+            if (nums[i] == nums[j])
+            {
+                res++;
+            }
+        }
+    }
+
+    return res;
+}
+
+int res = NumIdenticalPairs(nums);
+Console.WriteLine(res);
