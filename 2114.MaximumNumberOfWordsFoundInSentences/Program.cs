a36ad59e31f6dbce2b54fa847f3bc6b6782cbaeb
Vyacheslav Beatum <podarkiprod@gmail.com>
2022-08-27 12:51:50 +0300

    added 2114. task

---
+++ 2114.MaximumNumberOfWordsFoundInSentences/Program.cs
@@ -0,0 +1,20 @@
+﻿string[] sentences = { "alice and bob love leetcode", "i think so too",
+    "this is great thanks very much" };
+
+
+int MostWordsFound(string[] sentences)
+{
+    int[] nums = new int[sentences.Length];
+
+    for (int i = 0; i < sentences.Length; i++)
+    {
+        string[] words = sentences[i].Split(' ');
+        nums[i] += words.Length;
+    }
+
+    int finalNum = nums.Max();
+    return finalNum;
+}
+
+int final = MostWordsFound(sentences);
+Console.WriteLine(final);
