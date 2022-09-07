b8918b9a347b38109bfaeccb49dda54622a63ccc
Vyacheslav Beatum <podarkiprod@gmail.com>
2022-09-07 18:49:07 +0300

    added 1374 task

---
+++ 1374.GenerateAStringWithCharactersThatHaveOddCounts/Program.cs
@@ -0,0 +1,9 @@
+﻿int n = 11;
+
+string GenerateTheString(int n)
+{
+    return n % 2 == 0 ? new String('a', n - 1) + 'b' : new String('a', n);
+}
+
+string res = GenerateTheString(n);
+Console.WriteLine(res);
