c678e6071ec121640e778fdba4ae078d5f6441bf
Vyacheslav Beatum <podarkiprod@gmail.com>
2022-09-08 18:33:51 +0300

    added 217 task

---
+++ 217.ContainsDuplicate/Program.cs
@@ -0,0 +1,17 @@
+﻿int[] nums = { 1, 2, 3, 1 };
+
+bool ContainsDuplicate(int[] nums)
+{
+    for (int i = 0; i < nums.Length; i++)
+    {
+        for (int j = i + 1; j < nums.Length; j++)
+        {
+            if (nums[i] == nums[j])
+                return true;
+        }
+    }
+
+    return false;
+}
+
+bool b = ContainsDuplicate(nums);
