e6000527aaf922753397f9ca925e6ca13ae74d3f
Vyacheslav Beatum <podarkiprod@gmail.com>
2022-09-18 17:57:57 +0300

    added 268 & 383 tasks

---
+++ 268.MissingNumber/Program.cs
@@ -0,0 +1,19 @@
+﻿int[] nums = { 3, 0, 1 };
+
+int MissingNumber(int[] nums)
+{
+    Dictionary<int, int> dict = new Dictionary<int, int>();
+
+    for (int i = 0; i <= nums.Length; i++)
+    {
+        //Dictionary with right amount of numbers
+        dict.Add(i, 1);
+    }
+
+    foreach (var item in nums)
+    {
+        if (dict.ContainsKey(item))
+            dict.Remove(item);
+    }
+    return dict.Keys.First();
+}
---
+++ 383.RansomNote/Program.cs
@@ -0,0 +1,32 @@
+﻿string ransomNote = "aa";
+string magazine = "aab";
+
+bool CanConstruct(string ransomNote, string magazine)
+{
+    List<char> chars = new List<char>();
+    int num = 0;
+
+    for (int i = 0; i < magazine.Length; i++)
+    {
+        //Adding magazine chars to list
+        chars.Add(magazine[i]);
+    }
+
+    foreach (var c in ransomNote)
+    {
+        if (chars.Contains(c))
+        {
+            num++;
+            chars.Remove(c);
+        }
+    }
+
+    if (num == ransomNote.Length)
+        return true;
+    else
+        return false;
+}
+
+bool flag = CanConstruct(ransomNote, magazine);
+Console.WriteLine(flag);
+
