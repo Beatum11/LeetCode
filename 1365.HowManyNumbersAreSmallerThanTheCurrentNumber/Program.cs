49238d1e7b4c548d04ff9e77b0a1730417a81b92
Vyacheslav Beatum <podarkiprod@gmail.com>
2022-09-20 19:22:14 +0300

    added 1365 task

---
+++ 1365.HowManyNumbersAreSmallerThanTheCurrentNumber/Program.cs
@@ -0,0 +1,24 @@
+﻿int[] nums = { 6, 5, 4, 8 };
+
+
+int[] SmallerNumbersThanCurrent(int[] nums)
+{
+    int num = 0;
+    int[] finalArr = new int[nums.Length];
+
+    for (int i = 0; i < nums.Length; i++)
+    {
+        for (int j = 0; j < nums.Length; j++)
+        {
+            if (nums[i] > nums[j])
+                num += 1;
+        }
+
+        finalArr[i] = num;
+        num = 0;
+    }
+
+    return finalArr;
+}
+
+int[] final = SmallerNumbersThanCurrent(nums);
