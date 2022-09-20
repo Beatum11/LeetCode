bbe29c92ed8ea3df425d71fe241ce1e563067bb2
Vyacheslav Beatum <podarkiprod@gmail.com>
2022-09-20 15:59:09 +0300

    added 1684 and 412 tasks

---
+++ 1684.CountTheNumberOfConsistentStrings/Program.cs
@@ -0,0 +1,30 @@
+﻿string allowed = "ab";
+string[] words = { "ad", "bd", "aaab", "baa", "badab" };
+
+int CountConsistentStrings(string allowed, string[] words)
+{
+    int count = 0;
+    foreach (string word in words)
+    {
+        bool consistentWord = true;
+
+        foreach (char c in word)
+        {
+            //If index is less than 0 we are moving on to another word.
+            if (allowed.IndexOf(c) < 0)
+            {
+                consistentWord = false;
+                break;
+            }
+        }
+        if (consistentWord)
+        {
+            count++;
+        }
+    }
+
+    return count;
+}
+
+int res = CountConsistentStrings(allowed, words);
+Console.WriteLine(res);
